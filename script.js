1f889f01108bd15f2d487e7f2dd50d7293d9a61d
Sebastian Volder <sebastianvolder@Sebastians-MacBook-Pro-4.local>
2018-12-10 12:32:48 +0100

    gg

--- script.js
+++ script.js
@@ -1,35 +1,9 @@
 
 
-function myFunction(x) {
-    if (x.matches) { // If media query matches
-        let NewVid = function (movie) {
-    // create a <video>
-    let vid = document.createElement('video');
-    vid.controls = false;
-    vid.src = movie;
-    vid.autoplay = true;
-    vid.loop = true;
-    vid.muted = true;
-    vid.load();
-    vid.style.width = '100%';
-
-    // append to <div id="stemvid">
-    stemvid.appendChild(vid);
-}
-let forside = 'forsidemobil.mp4';
-// use the object
-NewVid(forside);
-
-let View = function (movie) {
-    let vid = document.createElement('video');
-    vid.controls = true;
-    vid.src = movie;
-    vid.load();
-    vid.style.width= '100%';
-    vid.style.zIndex= '-1';
-    interview.appendChild(vid);
-}
-    } else {
+// media query change
+function WidthChange(mq) {
+if (mq.matches) {
+// window width is at least 500px
         let NewVid = function (movie) {
     // create a <video>
     let vid = document.createElement('video');
@@ -57,9 +31,42 @@ let View = function (movie) {
     vid.style.zIndex= '-1';
     interview.appendChild(vid);
 }
+    } else {
+// window width is less than 500px
+
+
+        let NewVid = function (movie) {
+    // create a <video>
+    let vid = document.createElement('video');
+    vid.controls = false;
+    vid.src = movie;
+    vid.autoplay = true;
+    vid.loop = true;
+    vid.muted = true;
+    vid.load();
+    vid.style.width = '100%';
+
+    // append to <div id="stemvid">
+    stemvid.appendChild(vid);
+}
+let forside = 'forsidemobil.mp4';
+// use the object
+NewVid(forside);
+
+let View = function (movie) {
+    let vid = document.createElement('video');
+    vid.controls = true;
+    vid.src = movie;
+    vid.load();
+    vid.style.width= '100%';
+    vid.style.zIndex= '-1';
+    interview.appendChild(vid);
+}
     }
 }
 
-let x = window.matchMedia("(max-width: 700px)")
-myFunction(x) // Call listener function at run time
-x.replace(myFunction) // Attach listener function on state changes
+// media query event handler
+if (matchMedia) {
+let mq = window.matchMedia("(min-width: 500px)");
+WidthChange(mq);
+}
